33ceb606698400cdc7bfe684249d38ff64a39d7a
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-04-24 01:39:45 +0400

    Create 18-homework.swift

---
+++ 2-homework/18-homework.swift
@@ -0,0 +1,12 @@
+/*  HomeWork 2.18
+
+ Տրված են հետևյալ հաստատունը str = “Hello”
+ ջնջել str-ի առաջին սինվոլը
+
+ */
+let str :String = "Hello"
+var x :String = str
+
+x.remove(at: x.startIndex)
+
+print(x)
