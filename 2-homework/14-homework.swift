2f0a01ecd82da18c3d911ed6c77cc1d8b8aee8b4
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-04-24 01:37:34 +0400

    Create 14-homework.swift

---
+++ 2-homework/14-homework.swift
@@ -0,0 +1,16 @@
+/*  HomeWork 2.14
+
+ Տրված են հետևյալ հաստատունը str = “Hello”
+ տպել  str -ի մեջտեղում գտնվող  սինվոլը (Օգտագործել.   
+ subscrit սինտաքսը  -> [  ].  և  startIndex, index(_: offsetBy:  ) )
+
+*/
+let str :String = "Hello"
+
+let startIndex :String.Index = str.startIndex
+let strMiddle :Int = str.count / 2
+
+let offset :String.Index = str.index(startIndex, offsetBy: strMiddle)
+
+print(str[offset])
+
