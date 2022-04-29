590cec5d446095e1255503a1cc4cf0186c9f6f57
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-04-29 13:12:46 +0400

    Create 3.1-homework.swift

---
+++ 3-homework/3.1-homework.swift
@@ -0,0 +1,29 @@
+/*  HomeWork 3
+ 
+ Tuples, Optional Types
+
+ HomeWork 3_1
+
+ հայտարարել հաստատւներ a = 12,  b = -34.55, hiText = “Hello”, itIsBool = true,
+ նշված հաստատւները խմբաորել Tuple-ի միջոցով  all հաստատունում:
+ 
+*/
+
+let a: Int = 12
+let b: Double = -34.55
+let hiText: String = "Hello"
+let itIsBool: Bool = true
+
+//  example 1
+
+let all: (Int, Double, String, Bool) = (a, b, hiText, itIsBool)
+
+print(all)
+                            
+//  example 2
+
+let all_2 = (a, b, hiText, itIsBool)
+
+
+print(all_2)
+
