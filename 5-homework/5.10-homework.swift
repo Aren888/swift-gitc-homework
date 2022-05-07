118ab21ab0acac58a230b63948d016ea377879b1
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-05-07 13:08:17 +0400

    Update 5.10-homework.swift

--- 5-homework/5.10-homework.swift
+++ 5-homework/5.10-homework.swift
@@ -1,17 +1,25 @@
+import Darwin
+
 /*  Home Work 5_10
  Հայտարարել փոփոխական  a [String] տիպի հետևևայ արժեքներով [“Hello”, “iOS”, “GITC”]
  տպել մասիվի մեջտեղում գտնվող էլեմենտի մեջտեղում գտնվող սինվոլը եթե այն դատարկ չէ
  */
 
-private let items: [String] = ["Hello", "iOS", "GITC"]
 
-private func getItemsMiddleCharacter(_ items: [String]) -> Character? {
- 
-        if items.isEmpty || items.count%2 == 0 { return nil }
-        let middleItem = items[items.count/2]
- 
-        if middleItem.count%2 == 0 { return nil }
-        let middleCharacter: Character = middleItem[middleItem.index(middleItem.startIndex, offsetBy: middleItem.count/2)]
-        return middleCharacter
-    }
-print(getItemsMiddleCharacter(items)!)
+
+let items: [String] = ["Hello", "iOS", "GISDNVKSJDKNLTC","iOS","iOS"]
+
+if items.isEmpty || items.count%2 == 0 { print("nil") }
+let middleItem = items[items.count/2]
+if middleItem.count % 2 == 0 { print("nil") }
+let middleCharacter: Character = middleItem[middleItem.index(middleItem.startIndex, offsetBy: middleItem.count/2)]
+        print( middleCharacter)
+    
+
+
+
+
+
+
+
+
