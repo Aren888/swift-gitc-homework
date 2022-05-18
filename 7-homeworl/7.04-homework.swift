1daf793ee1713353f49da848ca4d5e25fa01db78
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-05-18 17:59:37 +0400

    Create 7.04-homework.swift

---
+++ 7-homeworl/7.04-homework.swift
@@ -0,0 +1,14 @@
+/*  Home Work 7_4
+ Հայտարարել Array հետևյալ արժեքով  [4, 54, 36, 127]
+ while  ցիկլի միջոցով հաշվել այդ  Array-ում գտնվող  կենտ թվերի գումարը
+
+*/
+let arr = [4, 54, 36, 127]
+var i = 0
+var sum = 0
+
+while i < arr.count {
+    if arr[i] % 2 != 0 { sum += arr[i] }
+    i += 1
+}
+print(sum)
