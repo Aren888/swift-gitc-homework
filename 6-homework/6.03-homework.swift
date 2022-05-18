649513aedf929272b3c43dfcbe46e0415f78358b
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-05-18 17:53:25 +0400

    Create 6.03-homework.swift

---
+++ 6-homework/6.03-homework.swift
@@ -0,0 +1,18 @@
+/*  Home Work 6_3
+ հայտարարել Int -երի Set
+ insert() ֆունկցիայի միջոցով ավելացնել հետևյալ արժեքները
+ 1, 3, 4, 5, 6,
+ For in ցիկլի միջոցով տպեք այդ Set - ում գտնվող արժեքները տպեք
+
+*/
+var mySet: Set<Int> = []
+mySet.insert(1)
+mySet.insert(2)
+mySet.insert(3)
+mySet.insert(4)
+mySet.insert(5)
+mySet.insert(6)
+
+for i in mySet {
+    print(i)
+}
