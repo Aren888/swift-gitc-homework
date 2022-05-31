5f604533557ee82e8716d60abbfd6d3ddb2f7d75
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-05-31 12:05:22 +0400

    Create 01.homework.swift

---
+++ 01-homework/01.homework.swift
@@ -0,0 +1,23 @@
+/*  HomeWork 1.1
+Հայտարարել փոփոխականեր(var) հետևյալ արժեքներով՝
+100, 399, 3.55, -5.66, 1, 0, -1, “String”, “Hello”
+ 
+print()-ի օգնությամբ տպել բոլոր փոփոխակաները
+*/
+
+var x1 :Int = 100
+var x2 :Int = 399
+var x3 :Double = 3.55
+var x4 :Double = -5.66
+var x5 :Int = 1
+var x6 :Int = 0
+var x7 :Int = -1
+var x8 :String = "String"
+var x9 :String = "Hello"
+
+print("x1 Variable type -> \(type(of: x1)) : x1 = \(x1)")
+print("x2 Variable type -> \(type(of: x2)) : x2 = \(x2)")
+print("x3 Variable type -> \(type(of: x3)) : x3 = \(x3)")
+print("x4 Variable type -> \(type(of: x4)) : x4 = \(x4)")
+print("x5 Variable type -> \(type(of: x5)) : x5 = \(x5)")
+print("x6 Variable type -> \(type(of: x6)) : x6 = \(x6)")
