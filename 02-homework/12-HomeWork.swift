c12c5078cd1b2e0aef8ad3716044b0b80624f175
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-05-31 12:58:52 +0400

    Create 12-HomeWork.swift

---
+++ 02-homework/12-HomeWork.swift
@@ -0,0 +1,8 @@
+/*  HomeWork 2.12
+ Տրված են հետևյալ հաստատունը str = “Hello”
+ տպել  str -ի առաջին սինվոլը  (Օգտագործել.   subscrit սինտաքսը  -> [  ].  և  StartIndex )
+ */
+
+let str: String = "Hello"
+
+print("This is first charecter of the word -> \(str[str.startIndex])")
