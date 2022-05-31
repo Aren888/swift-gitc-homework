66d9b0c1efcac4bc7529d4059a914afb02805f10
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-05-31 12:51:00 +0400

    Create 01-HomeWork.swift

---
+++ 02-homework/01-HomeWork.swift
@@ -0,0 +1,21 @@
+/*  Home Work 2
+ operators : Unary ( -a, +a, !a ),
+ binary ( + - * / % == != <= >= ),
+ ternary ( BOOL  ?  { returned action value } :  {returned action value}  )
+ String: property( isEmpty , count, startIndex, endIndex ),
+ function ( index() insert() remove()  )
+ HomeWork 2.1
+ Ունառնի օպերատոռի միջոցով փոխել հետեվյալ փոփոխակաների արժեքը
+ իրենց հակատարձ արժեքով  a = 5  b = 145, c = -1234.33, d = false,
+*/
+
+let a: Int = 5
+let b: Int = 145
+let c: Double = -123.33
+let d: Bool = false
+
+print("a = \(a) , -a = \(-a)")
+print("b = \(b) , -b = \(-b)")
+print("b = \(c) , -b = \(-c)")
+print("d = \(d) , !d = \(!d)")
+
