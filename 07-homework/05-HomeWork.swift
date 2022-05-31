3217b61f9031a88449b3e8cf03bf39adcb3ee990
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-05-31 13:43:59 +0400

    Create 05-HomeWork.swift

---
+++ 07-homework/05-HomeWork.swift
@@ -0,0 +1,15 @@
+/*  Home Work 7_5
+ Հայտարարել Array հետևյալ արժեքով  [4, 54, 36, 127]
+ while  ցիկլի միջոցով հաշվել այդ  Array-ում գտնվող  թվերի միջին թվաբանականը
+*/
+let arr =  [4, 54, 36, 127]
+var i = 0
+var sum = 0
+
+while i < arr.count {
+    sum += arr[i]
+    i += 1
+}
+let averege = sum / arr.count
+
+print(averege)
