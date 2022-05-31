163bb3cfc9767afeda9d03a5ccc4e3739916fa7b
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-05-31 13:38:58 +0400

    Create 10-HomeWork.swift

---
+++ 06-homework/10-HomeWork.swift
@@ -0,0 +1,15 @@
+/*  Home Work 6_10
+ For in ցիկլի միջոցով հաշվել 6 -ի ֆակտորիալը
+ ֆակտորիալը ուսումնասիրել
+*/
+
+let x = 3
+
+var result = 1
+var i = 1
+while i <= x {
+    result *= i
+    i += 1
+}
+
+print(result)
