9c4b07f622aec27fbdeecdc6e53c4fca479f3a40
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-05-31 13:51:32 +0400

    Delete 9-homework directory

--- 9-homework/9.5-homework.swift
+++
@@ -1,8 +0,0 @@
-/*  Home Work 9_5
- հայտարարել ֆունկցիա որը կվերադաձնի Int թվերի քառակուսին։
-*/
-
-func getSquareOfTheNumber (number : Int) -> Int {
-    return number * number
-}
-print("Square of the number -> \(getSquareOfTheNumber(number: 4))")
