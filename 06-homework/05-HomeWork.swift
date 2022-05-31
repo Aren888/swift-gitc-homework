afbe000307d64f3a2bd53006fb5f16f2644ccf43
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-05-31 13:36:13 +0400

    Create 05-HomeWork.swift

---
+++ 06-homework/05-HomeWork.swift
@@ -0,0 +1,12 @@
+/*  Home Work 6_5
+ Հայտարարել Array հետևյալ արժեքով  [4, 5, 6, 7]
+ For in ցիկլի միջոցով այդ Array-ում գտնվող աժեքները մեծացնել 2 անգամ
+*/
+
+var myArr = [4, 5, 6, 7]
+
+
+for (index, _ )in myArr.enumerated() {
+    myArr[index] = myArr[index] * 2
+}
+print(myArr)
