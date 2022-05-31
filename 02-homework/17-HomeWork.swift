b17d942e9bc2082942add390e622346eda081d0e
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-05-31 13:03:02 +0400

    Create 17-HomeWork.swift

---
+++ 02-homework/17-HomeWork.swift
@@ -0,0 +1,13 @@
+/*  HomeWork 2.17
+ Տրված են հետևյալ հաստատունը str = “Hello”
+ str-ի առաջին սինվոլից հետո ավելացնել “—Error—” տեքստը
+ (Օգտագործել.   insert(contentsOf: )  և  , index(after: ), startIndex) )
+*/
+
+let str: String = "Hello"
+var x: String = str
+let aftrerIndex = x.index(after: x.startIndex)
+
+x.insert(contentsOf: "-Error-", at: aftrerIndex)
+
+print(x)
