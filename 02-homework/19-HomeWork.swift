66164c0bbc637fef1a8c4f81db1161b8561771df
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-05-31 13:04:08 +0400

    Create 19-HomeWork.swift

---
+++ 02-homework/19-HomeWork.swift
@@ -0,0 +1,12 @@
+/*  HomeWork 2.19
+ Տրված են հետևյալ հաստատունը str = “Hello”
+ ջնջել str-ի վերջին սինվոլը
+ */
+
+let str :String = "Hello"
+var x :String = str
+let endIndex = x.index(before: x.endIndex)
+
+x.remove(at: endIndex)
+
+print(x)
