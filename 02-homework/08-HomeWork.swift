b60728c5d71b57ded4586ef50d631259914f81e5
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-05-31 12:55:58 +0400

    Create 08-HomeWork.swift

---
+++ 02-homework/08-HomeWork.swift
@@ -0,0 +1,14 @@
+/*  HomeWork 2.8
+ հայտարարել դատարկ String
+ ստուգել արդյոք դատարկ է
+*/
+
+let str: String = ""
+
+//      example 1
+var x: String = str.isEmpty ? "empty" : "not empty"
+print("String is \(x)")
+
+//      example 2
+x  = str.count == 0 ? "empty" : "not empty"
+print("String is \(x)")
