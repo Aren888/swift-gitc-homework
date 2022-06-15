79b6b78814da8b3b5adc4b47fa8868a34bb73e87
Aren888 <83555155+Aren888@users.noreply.github.com>
2022-06-15 11:11:58 +0400

    Create 08-HomeWork.swift

---
+++ 12-homework/08-HomeWork.swift
@@ -0,0 +1,27 @@
+/*  8. Ստեղծել կոճակի (Button) ըթացքիկ վիճակը բնութագրող տիպ, այս խնդրում  կոճակը ունի 3 վիճակ (case) երբ սեղմված չէ (normal), երբ սեղմված է (selected), երբ վոչ ակտիվ է,  normal վիճակում իրեն կապակցված արժեքը պետք է լինի այդ Button-ի անունը, selected վիճակում  իրեն կապակցված արժեքը պետք է լինի այն closure որը պետք է կանչվի սեղմելուց հետո։
+*/
+
+
+enum ButtonType {
+    case normal(nameOfButton: String)        //  երբ սեղմված չէ
+    case selected(handler: () -> Void)       //  երբ սեղմված է
+    case notActive     //  երբ վոչ ակտիվ է
+}
+
+
+var buttonName: String = "Button Name"
+
+var button: ButtonType = ButtonType.normal(nameOfButton: "main")
+
+button = ButtonType.selected(handler: { print("Selected") })
+
+switch button {
+case .normal(let nameOfButton):
+    print(nameOfButton)
+case ButtonType.selected(let handler):
+    handler()
+default:
+    break
+}
+
+
